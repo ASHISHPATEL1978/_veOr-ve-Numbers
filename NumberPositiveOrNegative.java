06a6fad6948b9e4030efebd30276bc5d2bf3591e
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 22:40:21 +0000

    -VE or +VE

---
+++ NumberPositiveOrNegative.java
@@ -0,0 +1,24 @@
+package checkpositivenegativenumber;
+
+public class NumberPositiveOrNegative
+{
+
+    public static void main(String[] args)
+    {
+
+        int value=-5;
+        if (value<0)
+        {
+            System.out.println ( value + " is Negative Number");
+
+        } else if (value>0)
+
+        {
+            System.out.println ( value + " is Positive Number");
+
+        } else
+        {
+            System.out.println ( value + " is Zero Number");
+        }
+    }
+}
